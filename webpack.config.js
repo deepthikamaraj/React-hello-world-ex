5b8f8ec11a003631483dd3fe2b4a1af5dfae330a
Deepthi K <deepthi.k2@cognizant.com>
2017-04-11 11:26:02 +0530

    first react

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+var config = {
+
+    entry: "./main.js",
+
+    output: {
+        path: path.resolve('./'),
+        filename: "index.js"
+    },
+
+    devServer: {
+        inline: true,
+        port: process.env.PORT || 3000
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+
+
+}
+
+module.exports = config;
